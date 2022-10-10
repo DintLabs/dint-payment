8e12e120dc617b23c38341574a501049ac93c141
maxxpa <info@dint.com>
2022-10-10 17:20:27 -0400

    Update stripe.js

--- pages/api/webhooks/stripe.js
+++ pages/api/webhooks/stripe.js
@@ -2,50 +2,42 @@ import Stripe from 'stripe';
 import { ethers } from 'ethers';
 import { buffer } from "micro";
 import axios from 'axios';
-
 const stripe = new Stripe(process.env.STRIPE_SECRET);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET; // validate requests
 
-async function transferDint({ amount, destAddr }) {
-    const provider = new ethers.providers.JsonRpcProvider(
-      process.env.JSON_RPC_URL // mumbai, polygon, eth mainnet
-    );
+const transferDint = async ({ amount, destAddr }) => {
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.JSON_RPC_URL // mumbai, polygon, eth mainnet
+  );
   // private key of account that holds DINT.
   // Caution: this account must have MATIC/ETH to cover gas fees!
   const signer =  new ethers.Wallet(
     process.env.DINT_OPERATOR_PRIVATE_KEY,
     provider
-  );
-    const abi = [
-        {
-          "constant": false,
-          "inputs": [
-            { "name": "_to", "type": "address" },
-            { "name": "_amount", "type": "uint256" }
-          ],
-          "name": "transfer",
-          "outputs": [{ "name": "success", "type": "bool" }],
-          "payable": false,
-          "stateMutability": "nonpayable",
-          "type": "function"
-        }
-      ]; 
+  )
+  const abi = [
+    {
+      "constant": false,
+      "inputs": [
+        { "name": "_to", "type": "address" },
+        { "name": "_amount", "type": "uint256" }
+      ],
+      "name": "transfer",
+      "outputs": [{ "name": "success", "type": "bool" }],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
 
+  const contractAddr = process.env.DINT_CONTRACT_ADDR;
+  const erc20dint = new ethers.Contract(contractAddr, abi, signer);
+  const tx = await erc20dint.transfer(
+    destAddr,
+    amount
+  ) // TRANSFER DINT to the customer
 
-       // get max fees from gas station
-      const maxFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
-       const maxPriorityFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei  
-    
-       const contractAddr = process.env.DINT_CONTRACT_ADDR;
-    const erc20dint = new ethers.Contract(contractAddr, abi, signer);
-    const tx = await erc20dint.transfer({
-       destAddr,
-        maxFeePerGas,
-        maxPriorityFeePerGas,
-       amount,
-    }) // TRANSFER DINT to the customer
-
-    return tx;
+  return tx;
 }
 
 export const config = {
@@ -57,7 +49,9 @@ export const config = {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-
+// get max fees from gas station
+let maxFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
+let maxPriorityFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
   try {
     const { data } = await axios({
       method: 'get',
@@ -65,11 +59,12 @@ export default async function handler(req, res) {
   })
   maxFeePerGas = ethers.utils.parseUnits(
       Math.ceil(data.fast.maxFee) + '',
-      'gwei',
+      'gwei'
   )
-  maxPriorityFeePerGas = ethers.ethers.utils.parseUnits(
+  maxPriorityFeePerGas = ethers.utils.parseUnits(
       Math.ceil(data.fast.maxPriorityFee) + '',
-      'gwei')
+      'gwei'
+  )
     const buf = await buffer(req);
     const sig = req.headers["stripe-signature"];
     // make sure that webhook is called by Stripe (not hackers or other ppl)
@@ -79,7 +74,11 @@ export default async function handler(req, res) {
       const amount = ethers.utils.parseEther(String(event.data.object.amount / 100))
       const destAddr = event.data.object.metadata.walletAddr;
       console.log({ amount, destAddr });
-      const tx = await transferDint({ amount, destAddr })
+      const tx = await transferDint({ 
+        amount, 
+        maxFeePerGas,
+        maxPriorityFeePerGas,
+        destAddr })
       console.log("tx hash", tx.hash);
     }
     res.status(200).json({ received: true });
