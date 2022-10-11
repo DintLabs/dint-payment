40fb220150c401410a8a9f7934a2857855cf9b77
maxxpa <info@dint.com>
2022-10-11 07:51:20 -0400

    Update stripe.js

--- pages/api/webhooks/stripe.js
+++ pages/api/webhooks/stripe.js
@@ -1,24 +1,20 @@
 import Stripe from 'stripe';
 import { ethers } from 'ethers';
 import { buffer } from "micro";
-import axios from 'axios';
+
 const stripe = new Stripe(process.env.STRIPE_SECRET);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET; // validate requests
-let maxFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
-let maxPriorityFeePerGas = ethers.BigNumber.from(40000000000) // fallback to 40 gwei
 
-
-async function transferDint({
-  amount, destAddr, maxFeePerGas }) {
+const transferDint = async ({ amount, destAddr }) => {
   const provider = new ethers.providers.JsonRpcProvider(
     process.env.JSON_RPC_URL // mumbai, polygon, eth mainnet
   );
   // private key of account that holds DINT.
   // Caution: this account must have MATIC/ETH to cover gas fees!
-  const signer = new ethers.Wallet(
+  const signer =  new ethers.Wallet(
     process.env.DINT_OPERATOR_PRIVATE_KEY,
     provider
-  );
+  )
   const abi = [
     {
       "constant": false,
@@ -38,9 +34,8 @@ async function transferDint({
   const erc20dint = new ethers.Contract(contractAddr, abi, signer);
   const tx = await erc20dint.transfer(
     destAddr,
-    maxFeePerGas,
     amount
-  ); // TRANSFER DINT to the customer
+  ) // TRANSFER DINT to the customer
 
   return tx;
 }
@@ -54,10 +49,8 @@ export const config = {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-// get max fees from gas station
 
   try {
-    await newFunction();
     const buf = await buffer(req);
     const sig = req.headers["stripe-signature"];
     // make sure that webhook is called by Stripe (not hackers or other ppl)
@@ -67,11 +60,7 @@ export default async function handler(req, res) {
       const amount = ethers.utils.parseEther(String(event.data.object.amount / 100))
       const destAddr = event.data.object.metadata.walletAddr;
       console.log({ amount, destAddr });
-      const tx = await transferDint({ 
-        amount, 
-        maxFeePerGas,
-        maxPriorityFeePerGas,
-        destAddr })
+      const tx = await transferDint(amount, destAddr, {gasPrice: ethers.utils.parseUnits('100', 'gwei'), gasLimit: 1000000});
       console.log("tx hash", tx.hash);
     }
     res.status(200).json({ received: true });
@@ -79,18 +68,3 @@ export default async function handler(req, res) {
     res.status(400).send(`Webhook Error: ${err.message}`);
   }
 }
-async function newFunction() {
-  const { data } = await axios({
-    method: 'get',
-    url: 'https://gasstation-mainnet.matic.network/v2',
-  });
-  maxFeePerGas = ethers.utils.parseUnits(
-    Math.ceil(data.fast.maxFee) + '',
-    'gwei'
-  );
-  maxPriorityFeePerGas = ethers.utils.parseUnits(
-    Math.ceil(data.fast.maxPriorityFee) + '',
-    'gwei'
-  );
-}
-
